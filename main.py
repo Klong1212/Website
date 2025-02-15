0ebf092ac0ac8e4dd08f01ec2e90a2c7a3a4767e
Tanada Nawakata <klong8257@gmail.com>
2025-02-16 00:21:42 +0700

    complete sign in?

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='bootstrap', static_url_path='/bootstrap')
+
 @app.route('/')
 def main():
     return render_template('index.html')
+
 @app.route('/signin')
 def login():
     return render_template('signin.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
